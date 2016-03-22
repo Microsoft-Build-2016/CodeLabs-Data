5f42b0392e704d530a12a2fa2d28f935e7646d7e
jonicis <jonathan.cisneros@southworks.net>
2016-03-22 12:47:46 -0300

    removed settings

--- Module1-IntelligentApp/Source/Ex1/Assets/ProductCatalogSampleData/ProductCatalogSampleData/Program.cs
+++ Module1-IntelligentApp/Source/Ex1/Assets/ProductCatalogSampleData/ProductCatalogSampleData/Program.cs
@@ -6,8 +6,8 @@
     public class Program
     {
         static string documentDbName = "PartsUnlimited";
-        static string documentDbEndpoint = "https://datamodule1.documents.azure.com:443/";
-        static string documentDbAuthKey = "Xfa0s13Fyq6/FXuZLUyi5yS5c0JB9KD58Ech7Vl/q4bU0L7jhmOOFHipmLDFzYE5r4QNkw09+IS9SN1xLpCiJQ==";
+        static string documentDbEndpoint = "";
+        static string documentDbAuthKey = "";
         static string documentDbCollection = "Products";
 
         static void Main(string[] args)
