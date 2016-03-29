8d46c65710b1066b3090ead2c3140e1099520c9c
Romit Girdhar <romitgirdhar@outlook.com>
2016-03-28 21:42:56 -0400

    Adding Putty and creating a binary for ProductCatalogUploader

--- Module1-IntelligentApp/Source/Ex1/Assets/ProductCatalogSampleData/ProductCatalogSampleData/Program.cs
+++ Module1-IntelligentApp/Source/Ex1/Assets/ProductCatalogSampleData/ProductCatalogSampleData/Program.cs
@@ -5,18 +5,20 @@
 
     public class Program
     {
-        static string documentDbName = "PartsUnlimited";
+       static string documentDbName = "PartsUnlimited";
         static string documentDbEndpoint = "";
         static string documentDbAuthKey = "";
         static string documentDbCollection = "Products";
 
         static void Main(string[] args)
         {
+
             Console.WriteLine("Initializing DocumentDB");
 
             DocumentDBTestData.InitializePartsUnlimitedDatabaseAsync(documentDbName, documentDbEndpoint, documentDbAuthKey, documentDbCollection).Wait();
 
             Console.WriteLine("DocumentDB data is ready");
+            Console.WriteLine("Press Any key to exit...");
             Console.ReadLine();
         }        
     }
